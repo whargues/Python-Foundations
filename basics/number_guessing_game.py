41d38333f0e4252ec79dfad474a90aacb54eaa2f
whargues <wyatthargues@gmail.com>
2026-06-04 20:51:20 -0500

    Add files via upload

---
+++ basics/number_guessing_game.py
@@ -0,0 +1,22 @@
+# This is the guess the number game.
+import random
+print('Hello! What is your name?')
+name = input()
+secret_number = random.randint(1, 20)
+print(f'Well {name}, I am thinking of a number between 1 and 20.')
+
+#Ask the player to guess 6 times.
+for guesses in range (1, 7):
+    print('Take a guess')
+    guess = int(input())
+    if guess < secret_number:
+        print('Your guess is to low')
+    elif guess > secret_number:
+        print('Your guess is to high')
+    else:
+        break # This condition is the correct guess!!
+
+if guess == secret_number:
+    print(f'Good job {name}! You guessed my number in ' + str(guesses)+ ' guesses')
+else:
+    print(f'{name}, you didnt guess the correct number')
